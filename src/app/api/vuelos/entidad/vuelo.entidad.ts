adc090015c838631effb135aaa9d254b59f36b98
Criss117 <cristianviveros@gmail.com>
2023-11-19 12:35:41 -0500

    api/vuelos/get/all implementado

---
+++ src/app/api/vuelos/entidad/vuelo.entidad.ts
@@ -0,0 +1,48 @@
+import oracleDB from "oracledb";
+import { getCursorInfo } from "../../repository";
+
+type vuelo = {
+  id: number;
+  aeropuerto_salida_id: number;
+  aeropuerto_llegada_id: number;
+  avion_id: number;
+  destino: string;
+  fecha_salida: string;
+  fecha_llegada: string;
+  cantidad_pasajeros: number;
+};
+
+export class Vuelo {
+  private id: number = -1;
+  private aeropuerto_salida_id: number = -1;
+  private aeropuerto_llegada_id: number = -1;
+  private avion_id: number = -1;
+  private destino: string = "";
+  private fecha_salida: string = "";
+  private fecha_llegada: string = "";
+  private cantidad_pasajeros: number = -1;
+
+  private connection: oracleDB.Connection | null = null;
+  constructor(newConnection: any) {
+    this.connection = newConnection;
+  }
+
+  public async getAll() {
+    if (!this.connection) {
+      return;
+    }
+    const sql = `
+      DECLARE
+      BEGIN
+        vuelo_crud.leer_vuelos(:cursor);
+      END;
+    `;
+    const cursor = { dir: oracleDB.BIND_OUT, type: oracleDB.CURSOR };
+    const result = await this.connection.execute(sql, { cursor });
+
+    //@ts-ignore
+    const response = await getCursorInfo(result.outBinds.cursor);
+    await this.connection.close();
+    return response;
+  }
+}
